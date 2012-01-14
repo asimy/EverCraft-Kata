4a5236eeecbe5955a8c9c77eeb75f979f877809f
Jim Weirich <jim.weirich@gmail.com>
2012-01-14 11:30:19 -0500

    Refactor: library level require file.

---
+++ ruby/lib/evercraft.rb
@@ -0,0 +1,13 @@
+require 'integer_extensions'
+
+require 'character'
+require 'attack'
+
+require 'standard_class'
+require 'fighter_class'
+require 'rogue_class'
+require 'war_monk_class'
+require 'paladin_class'
+
+require 'human_race'
+require 'orc_race'
--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,7 +1,3 @@
 require 'rspec/given'
 
-require 'character'
-require 'fighter_class'
-require 'rogue_class'
-require 'war_monk_class'
-require 'paladin_class'
+require 'evercraft'
